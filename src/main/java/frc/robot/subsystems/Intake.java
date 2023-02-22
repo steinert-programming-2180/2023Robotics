f7efa1627f0a2be1f60b6b23f0560024404f243c
NumberC <fadi50045000@gmail.com>
2023-02-21 23:29:52 -0500

    Refactored Intake and removed dual-motor functionality

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,29 +1,21 @@
 
 package frc.robot.subsystems;
 
-import java.lang.ModuleLayer.Controller;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-
-  CANSparkMax leftMotor;
-  CANSparkMax rightMotor;
-  XboxController xbox;
-  double speed = .5;
-
+  CANSparkMax clawMotor;
 
   public Intake() {
-    leftMotor = new CANSparkMax(0, MotorType.kBrushless);
-    rightMotor = new CANSparkMax(1, MotorType.kBrushless);
-    xbox = new XboxController(0);
-  } 
+    clawMotor = new CANSparkMax(IntakeConstants.intakeMotorID, MotorType.kBrushless);
+  }
+
   /**
    * Example command factory method.
    *
@@ -37,32 +29,28 @@ public class Intake extends SubsystemBase {
           /* one-time action goes here */
         });
   }
-  public void intake(){
-    while(xbox.getAButton()){
-      leftMotor.set(speed);
-      rightMotor.set(speed);
-    } 
 
-  }
-  public void outtake(){
-    while(xbox.getBButton()){
-      leftMotor.set(speed*-1);
-      rightMotor.set(speed*-1);
-    } 
-
+  public void intake() {
+    clawMotor.set(IntakeConstants.intakeSpeed);
   }
 
+  public void outtake() {
+    clawMotor.set(-IntakeConstants.intakeSpeed);
+  }
+
+  // TODO: how are we sensing if the claw has grabbed something? Encoders? Test out encoders.
+  // public boolean hasFieldElement(){
+  //   return true;
+  // }
 
   @Override
   public void periodic() {
-
   }
 
   @Override
   public void simulationPeriodic() {
-
   }
 
-  public void intakeOn(){
+  public void intakeOn() {
   }
 }
