96c7618b7f30cd73eaae82e999ec224782f1c758
Zero Gravity Programming <steinertrobotics@gmail.com>
2023-02-25 15:38:02 -0500

    added auto movement

---
+++ src/main/java/frc/robot/commands/Paths.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+
+public class Paths {
+    public static Trajectory edgeToCube () {
+        return TrajectoryGenerator.generateTrajectory(null,
+         null,
+          null, 
+        null);
+    }
+}
